9433f0976c6275e979fb33a5846e48601bfb3114
Giannis Karastatiras <gianniskarastatiras@gmail.com>
2019-01-04 19:41:48 +0200

    Add files via upload

---
+++ FirstHtmlDemo/FirstHtmlDemo/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FirstHtmlDemo.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server"><!-- Idiothta na postaretai sto server!--->
+        <div>
+            Enter your Age:
+            <br />
+            <input id="age" type="text" runat="server"/><!---ftiaxnei ena textbox!-->
+            <br />
+            <input id="Submit" type="button" value="Submit" runat="server" /><!--id gia to code kai value to ti tha deixnei to koumpi!--->
+            <br />
+            <div id="Message" runat="server"></div><!--runat Ginetai diathesimo sto server!--->
+        </div>
+    </form>
+</body>
+</html>
---
+++ FirstHtmlDemo/FirstHtmlDemo/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstHtmlDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //Method akolouthei to convention twn events,dld event pou analamvanei otan fortwnei h selida
+        protected void Page_Load(object sender, EventArgs e)
+        {//grafw ston server gia auto vlepei to button
+            Submit.ServerClick += Submit_ServerClick;//egrapsa to serverclick otan fortwsei h selida gia na yparxei
+        }//egrafw to event
+
+        //id tou control_eventName---onomasia
+        private void Submit_ServerClick(object sender, EventArgs e)//otan paththei to koumpi tha klhthei ayth
+        {
+            Message.InnerHtml = age.Value;
+        }
+    }
+}
